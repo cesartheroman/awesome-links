a6bce16c685d4781ae261e3092077188a78ac279
Cesar Roman <cesardroman18@gmail.com>
2023-06-23 12:29:21 -0500

    implemented auth0

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -2,10 +2,13 @@ import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import RelayPlugin from '@pothos/plugin-relay';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
+
 import prisma from '../lib/prisma';
+import { createContext } from './context';
 
 export const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes;
+  Context: ReturnType<typeof createContext>;
 }>({
   plugins: [PrismaPlugin, RelayPlugin],
   relayOptions: {},
